e8c7c872cd64abed258d60a3d03d0366a69d7e21
siber15 <siber15@users.noreply.github.com>
2019-04-03 11:59:22 +0200

    Add files via upload
    Interrupt Trigger added.

---
+++ MDB_Tachometer-SVB-2019.ino
@@ -0,0 +1,49 @@
+// source of code: http://www.pyroelectro.com/tutorials/tachometer_rpm_arduino/software.html
+// trigger: https://forum.arduino.cc/index.php?topic=155270.0 
+
+volatile int trigger = 0;
+volatile float time = 0;
+volatile float time_last = 0;
+volatile int rpm_array[5] = {0,0,0,0,0};
+int rpm = 0;
+
+void setup()
+{
+  //Digital Pin 2 Set As An Interrupt
+ attachInterrupt(0, my_interrupt, FALLING);
+
+}
+
+//Main Loop To Calculate RPM
+void loop()
+{
+  ////lcd.setCursor(4, 1);
+  ////lcd.print(time);   
+
+  
+  {    
+    //Last 3 Average RPM Counts Eqauls....
+      if (trigger > 0) 
+          {rpm = (rpm_array[0] + rpm_array[1] + rpm_array[2] + rpm_array[3] + rpm_array[4]) / 5;
+          }
+      }
+      
+      // print(rpm);   
+      trigger = 0;
+}
+ 
+
+//Capture Interrupt
+void my_interrupt()
+{
+   time = (micros() - time_last); 
+   time_last = micros();
+
+      rpm_array[0] = rpm_array[1];
+      rpm_array[1] = rpm_array[2];
+      rpm_array[2] = rpm_array[3];
+      rpm_array[3] = rpm_array[4];
+      rpm_array[4] = 60*(1000000/(time));    
+     
+ trigger++;
+}
