66ea9c247843fb08706efce462a41a470cc982a1
siber15 <siber15@users.noreply.github.com>
2019-04-24 15:27:03 +0200

    Add files via upload

--- MDB_Tachometer-SVB-2019.ino
+++ MDB_Tachometer-SVB-2019.ino
@@ -1,14 +1,17 @@
 // source of code: http://www.pyroelectro.com/tutorials/tachometer_rpm_arduino/software.html
 // trigger idea: https://forum.arduino.cc/index.php?topic=155270.0 
 
+volatile unsigned long time;
 volatile int trigger = 0;
-volatile float time = 0;
-volatile float time_last = 0;
-volatile int rpm_array[5] = {0,0,0,0,0};
-int rpm = 0;
+volatile unsigned long int t = 0;
+volatile unsigned long int time_last = 0;
+volatile float rpm_array[5] = {0,0,0,0,0};
+unsigned long int rpm = 0;
+volatile int adjust = 6;
 
 void setup()
 {
+  Serial.begin(9600);
   //Digital Pin 2 Set As An Interrupt
  attachInterrupt(0, my_interrupt, FALLING);
 
@@ -20,31 +23,41 @@ void loop()
   ////lcd.setCursor(4, 1);
   ////lcd.print(time);   
 
-  
   {    
-    //Last 3 Average RPM Counts Eqauls....
-      if (trigger < 5) 
+    //Last 5 Average RPM Counts Eqauls....
+      if (trigger >= 5) 
           {rpm = (rpm_array[0] + rpm_array[1] + rpm_array[2] + rpm_array[3] + rpm_array[4]) / 5;
+          
+          Serial.print(rpm);
+          Serial.print("  ");
           trigger = 0;
           }
-      }
-      
-      // print(rpm);   
-      
+      }   
 }
  
 
 //Capture Interrupt
 void my_interrupt()
 {
-   time = (micros() - time_last); 
-   time_last = micros();
+   time = millis();
+   t = ((millis() - time_last)); // Stores time in ms between two interrupts 
+   time_last = millis(); // Time in ms from program start
+   //Serial.print(t);
+   //Serial.print("  ");
+   
 
+
+   // Queue array
+      //if (t > 0) {
       rpm_array[0] = rpm_array[1];
       rpm_array[1] = rpm_array[2];
       rpm_array[2] = rpm_array[3];
       rpm_array[3] = rpm_array[4];
-      rpm_array[4] = 60*(1000000/(time));    
+      rpm_array[4] = 100000/t;  //converting to 100 Hz  
+      //Serial.print (rpm_array[1,2,3,4,5] \r);
+      //Serial.print("  ");
+      //}
+
      
  trigger++;
 }
