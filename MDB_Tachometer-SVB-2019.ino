867d77366678cc7406c8a0ade81e518e0c3e6cbb
siber15 <siber15@users.noreply.github.com>
2019-06-10 10:13:36 +0200

    Tilrettet til rapport

--- MDB_Tachometer-SVB-2019.ino
+++ MDB_Tachometer-SVB-2019.ino
@@ -1,13 +1,10 @@
 // source of code: http://www.pyroelectro.com/tutorials/tachometer_rpm_arduino/software.html
 // trigger idea: https://forum.arduino.cc/index.php?topic=155270.0 
 
-volatile unsigned long time;
-volatile int trigger = 0;
 volatile unsigned long int t = 0;
 volatile unsigned long int time_last = 0;
 volatile float rpm_array[5] = {0,0,0,0,0};
 volatile unsigned long int rpm = 0;
-volatile int adjust = 6;
 
 void setup()
 {
@@ -17,7 +14,7 @@ void setup()
 
 }
 
-//Main Loop To Calculate RPM
+//Main Loop
 void loop()
 {
 }
@@ -26,25 +23,22 @@ void loop()
 //Capture Interrupt
 void my_interrupt()
 {
-   time = millis();
-   t = ((millis() - time_last)); // Stores time in ms between two interrupts 
+   t = ((millis() - time_last)); // Stores to t, ms between two interrupts 
    time_last = millis(); // Time in ms from program start
    //Serial.print(t);
    //Serial.print("  ");
-   
-
 
    // Queue array
-      //if (t > 0) {
+      //if (t > 0) { // originally, we had a trigger "t"
       rpm_array[0] = rpm_array[1];
       rpm_array[1] = rpm_array[2];
       rpm_array[2] = rpm_array[3];
       rpm_array[3] = rpm_array[4];
       rpm_array[4] = 100000/t;  //converting to 100 Hz  
-      //Serial.print (rpm_array[1,2,3,4,5] \r);
+      //Serial.print (rpm_array[1,2,3,4,5] \r); // For testing in beginning
       //Serial.print("  ");
       //}
   
-      //Last 5 Average RPM Counts Eqauls.... (first 5 results are weird - but easier to process)
+      //Last 5 Average RPM Counts Eqauls.... (first 5 results are weird/wrong - but this way makes it easier to process)
           rpm = (rpm_array[0] + rpm_array[1] + rpm_array[2] + rpm_array[3] + rpm_array[4]) / 5;
 }
